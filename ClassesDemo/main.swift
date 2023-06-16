a89f867547e976cf4598c6cc3b3666be87e972ee
Dixon A <dixona2023@icloud.com>
2023-06-16 17:22:05 +0530

    " When to use A Class"

--- ClassesDemo/main.swift
+++ ClassesDemo/main.swift
@@ -2,8 +2,14 @@
 
 let skeleton = Enemy(health: 100, hitPoint: 10)
 
-//let skeleton01 = skeleton // instead of creating a new object, what we did here is simply referring to the same object.
-let skeleton01 = Enemy(health: 100, hitPoint: 10)
+let skeleton01 = skeleton
+
+
 skeleton.tookDamage(amount: 10)
+skeleton01.tookDamage(amount: 10)
+
 print(skeleton01.health)
 print(skeleton.health)
+// so what happens here is , instead of making changes to the separate variables, because of the variables hold the same object with reference to the same class . value get changed over and over.
+
+// so unless we don't want the func like inheritance , subclass , superclass. Always stick with a Struct
