85877de6d92f51df8d3c583ff6563253253bf0ae
Dixon A <dixona2023@icloud.com>
2023-06-16 17:09:34 +0530

    "Class is reference "

--- ClassesDemo/Enemy.swift
+++ ClassesDemo/Enemy.swift
@@ -1,14 +1,23 @@
 
 class Enemy {
-    let health = 100
-    let hitPoint = 10
+    var health : Int
+    var hitPoint : Int
+    
+    init(health: Int, hitPoint: Int) {
+        self.health = health
+        self.hitPoint = hitPoint
+    }
     
     func attack () {
-        print("attacks enemy with \(hitPoint) and loses \(health - 20)")
+        print("attacks enemy with \(hitPoint) hit points")
     }
     
     func move () {
         print("Walks forward")
     }
     
+    func tookDamage (amount : Int) {
+        health = health - amount
+        
+    }
 }
--- ClassesDemo/main.swift
+++ ClassesDemo/main.swift
@@ -1,10 +1,9 @@
 /* the point is Dragon has access to all props and methods of Enemy Superclass. In addition Dragon can have additional func . */
 
-let skeleton = Enemy()
+let skeleton = Enemy(health: 100, hitPoint: 10)
 
-let dragon = Dragon()
-dragon.wingspan = 20
-print(dragon.health)
-dragon.attack()
-dragon.move()
-dragon.talks(speech: "Dracarys")
+//let skeleton01 = skeleton // instead of creating a new object, what we did here is simply referring to the same object.
+let skeleton01 = Enemy(health: 100, hitPoint: 10)
+skeleton.tookDamage(amount: 10)
+print(skeleton01.health)
+print(skeleton.health)
